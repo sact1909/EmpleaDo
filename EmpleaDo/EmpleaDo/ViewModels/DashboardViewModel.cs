39937cb0564e9b0dff7c38a18a97ff8569cb7799
Steven Alexander Checo Terrero <steven.checo.19@gmail.com>
2019-11-16 17:52:03 -0500

    Cambios con MVVM

--- EmpleaDo/EmpleaDo/ViewModels/DashboardViewModel.cs
+++ EmpleaDo/EmpleaDo/ViewModels/DashboardViewModel.cs
@@ -3,35 +3,36 @@ using Flurl;
 using Flurl.Http;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace EmpleaDo
+namespace EmpleaDo.ViewModels
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public class DashboardViewModel : INotifyPropertyChanged
     {
-        public MainPage()
-        {
-            InitializeComponent();
-            testc();
+        public ObservableCollection<Job> Jobss { get; set; }
+        public Command LlenarListaCommand { get; set; }
+        public DashboardViewModel() {
+
+            new Action(async () => await cargarDatos())();
+           
+
         }
 
-        public async void testc() {
-            // Flurl will use 1 HttpClient instance per host
+
+        public async Task cargarDatos() {
             var Cards = await "https://emplea-apm.azure-api.net/v1/api"
                 .AppendPathSegment("jobs")
                 .SetQueryParams(new { pagesize = 10, page = 1 })
                 .WithHeader("Ocp-Apim-Subscription-Key", "155d4cd48bb34ba0b8375fd50b779b85")
                 .GetJsonAsync<JobsList>();
 
-            ListaEjemplo.HasUnevenRows = true;
-            ListaEjemplo.ItemsSource = Cards.Jobs;
+            Jobss = new ObservableCollection<Job>(Cards.Jobs);
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ EmpleaDo/EmpleaDo/Views/Dashboard.xaml.cs
@@ -0,0 +1,25 @@
+﻿using EmpleaDo.Models;
+using Flurl;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmpleaDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
